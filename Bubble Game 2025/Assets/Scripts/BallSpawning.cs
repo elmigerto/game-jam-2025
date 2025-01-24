cd12ddb60a07a654d88f3cbdf4abded8775dde38
Daniel <zbindendaniel92@gmail.com>
2025-01-24 20:58:29 +0100

    dani

---
+++ Bubble Game 2025/Assets/Scripts/BallSpawning.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+
+public class BallSpawning : MonoBehaviour
+{
+    public GameObject BallPrefab;
+
+    private System.Random rnd = new System.Random();
+    public float interval = 2f; // Time interval in seconds
+    public int force = 8;
+    private float timer = 0f;  // Tracks elapsed time
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Increment timer by the time elapsed since the last frame
+        timer += Time.deltaTime;
+
+        // Check if the timer has reached or exceeded the interval
+        if (timer >= interval)
+        {
+            timer -= interval; // Reset timer (keep remainder for accuracy)
+            InstantiateBall();        // Call the desired method
+        }
+
+        if (Time.deltaTime > 1)
+        {
+            InstantiateBall();
+        }
+
+    }
+
+    private void InstantiateBall()
+    {
+        var currentBall = Instantiate(BallPrefab, this.transform.position + new Vector3(0, 2, 0), new Quaternion());
+        var vX = rnd.Next(-force,force);
+        var vY = 3;
+        var vZ = rnd.Next(-force,force);
+        currentBall.GetComponent<Rigidbody>().linearVelocity = new Vector3(vX, vY, vZ);//.AddForce(new Vector3(1.2f,10.5f,5.6f));//.transform.v    }
+    }
+    }

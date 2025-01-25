e9fcb0b08a80a384a5ed74c685372852c8d488d4
Daniel <zbindendaniel92@gmail.com>
2025-01-25 14:10:06 +0100

    plant stuff

--- Bubble Game 2025/Assets/Scripts/SeedBehaviour.cs
+++ Bubble Game 2025/Assets/Scripts/SeedBehaviour.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class SeedBehaviour : MonoBehaviour
@@ -5,11 +6,10 @@ public class SeedBehaviour : MonoBehaviour
     public int despawnCount = 3;
     public GameObject arena;
     public GameObject plantBody;
+    public float bounceForce = 30;
 
     void OnCollisionEnter(Collision collision)
     {
-        // collision with a other player
-        // Debug.Log("Collision:  " + collision.gameObject.name);
         if (collision.gameObject.name == arena.name)
         {
             Debug.LogWarning("here");
@@ -17,11 +17,18 @@ public class SeedBehaviour : MonoBehaviour
             if (despawnCount == 0)
             {
                 BecomePlant();
+            }else{
+                BounceFromGround();
             }
         }
         else{
             Debug.Log("hit that" + collision.gameObject.name);
         }
+    }
+
+    private void BounceFromGround()
+    {
+        this.GetComponent<Rigidbody>().AddForce(Vector3.up * bounceForce);
     }
 
     private void BecomePlant()

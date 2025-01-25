bd1d375e1058381ea9320e0a1b4be5774cc1d72c
Daniel <zbindendaniel92@gmail.com>
2025-01-25 17:28:25 +0100

    dani

--- Bubble Game 2025/Assets/Scripts/PlayerMovement.cs
+++ Bubble Game 2025/Assets/Scripts/PlayerMovement.cs
@@ -143,5 +143,6 @@ public class PlayerMovement : MonoBehaviour
     {
         isGrounded = false;
         Debug.Log("Destroying");
+        GameManager.OnPlayerDestroyed(this.gameObject);
     }
 }

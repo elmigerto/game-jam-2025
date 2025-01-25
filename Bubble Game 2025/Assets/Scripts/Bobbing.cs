f3bd196ac45883d59e2de872adae5d8afcc17f07
Anic Mueri <anic.mueri@zhdk.ch>
2025-01-25 18:50:00 +0100

    First Batch of Assets

--- Bubble Game 2025/Assets/Scripts/Bobbing.cs
+++ Bubble Game 2025/Assets/Scripts/Bobbing.cs
@@ -1,15 +1,16 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.Mathf;
 
 public class Bobbing : MonoBehaviour
 {
+    [SerializeField] float bobbingOffset = 0;
+    [SerializeField] float bobbingAmount = 0.1f;
     private float bobbingInterval = .3f; //in seconds
-
-    // Update is called once per frame
     void Update()
     {
-        float newScale = (Pow(Sin((Time.time/bobbingInterval-0.5f)*PI)+1,1.6f))/2;
-        newScale = Lerp(newScale,1,0.9f); //remap
+        float newScale = (Pow(Sin((Time.time/bobbingInterval+bobbingOffset-0.5f)*PI)+1,1.6f))/2; //curve
+        newScale = Lerp(newScale,1,1-bobbingAmount); //remap
         transform.localScale = new Vector3(2-newScale,newScale,2-newScale);
     }
 }

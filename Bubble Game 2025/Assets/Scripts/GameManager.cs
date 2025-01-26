7ef9c1958d84b9cb506d58bb3dcf6b743c414802
elmigerto <102852964+elmigerto@users.noreply.github.com>
2025-01-26 14:34:21 +0100

    Add starting infos

--- Bubble Game 2025/Assets/Scripts/GameManager.cs
+++ Bubble Game 2025/Assets/Scripts/GameManager.cs
@@ -22,12 +22,16 @@ public class GameManager : MonoBehaviour
 
     public bool IsGameRunning { get; private set; }
 
+ 
+
 
     [Header("UI Settings")]
     public TextMeshProUGUI scoreTMPText; // For TextMeshPro
     public TextMeshProUGUI p1text; // For TextMeshPro
     public TextMeshProUGUI p2text; // For TextMeshPro
     public TextMeshProUGUI levelText;
+    public GameObject StartingInfos;
+    public float DisableInfosTime = 2f;
 
     [Header("Audio Settings")]
     public AudioClip backgroundMusic; // Assign your background music in the Inspector
@@ -50,6 +54,7 @@ public class GameManager : MonoBehaviour
         audioSource.volume = musicVolume;
 
         PlayBackgroundMusic();
+        RestartGame();
     }
 
     void Update()
@@ -189,5 +194,24 @@ public class GameManager : MonoBehaviour
     internal static void UpdateLevel(int v)
     {
         Instance.levelText.text = v.ToString();
+    }
+
+    void RestartGame()
+    {
+        ResetGame();
+        EnableStartingInfos();
+        Invoke("DisableStartingInfos", DisableInfosTime);
+    }
+
+
+    void EnableStartingInfos()
+    {
+        StartingInfos.SetActive(true);
+    }
+
+
+    void DisableStartingInfos()
+    {
+        StartingInfos.SetActive(false);
     }
 }

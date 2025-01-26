3053f3eb25024257c60a1fea2c0dcbd66b44984e
Anic Mueri <anic.mueri@zhdk.ch>
2025-01-26 11:38:45 +0100

    Assets Batch 3

--- Bubble Game 2025/Assets/Scripts/Bobbing.cs
+++ Bubble Game 2025/Assets/Scripts/Bobbing.cs
@@ -6,11 +6,21 @@ public class Bobbing : MonoBehaviour
 {
     [SerializeField] float bobbingOffset = 0;
     [SerializeField] float bobbingAmount = 0.1f;
+    [SerializeField] private bool doRandomOffset = true;
     private float bobbingInterval = .3f; //in seconds
+    private float randomOffsetAmount = 0;
+    private Vector3 initialScale;
+    void Start(){
+        if (doRandomOffset)
+        {
+            randomOffsetAmount = Random.Range(0f,1f);
+        }
+        initialScale = transform.localScale;
+    }
     void Update()
     {
-        float newScale = (Pow(Sin((Time.time/bobbingInterval+bobbingOffset-0.5f)*PI)+1,1.6f))/2; //curve
+        float newScale = (Pow(Sin((Time.time/bobbingInterval+randomOffsetAmount+bobbingOffset-0.5f)*PI)+1,1.6f))/2; //curve
         newScale = Lerp(newScale,1,1-bobbingAmount); //remap
-        transform.localScale = new Vector3(2-newScale,newScale,2-newScale);
+        transform.localScale = Vector3.Scale(new Vector3(2-newScale,newScale,2-newScale),initialScale);
     }
 }
